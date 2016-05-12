1a0eeeef825645da14e3011918f16d517d1b6aa3
xuxiaomeng <xuxiaomeng@baozou.com>
2016-05-12 18:27:43 +0800

    feature. update base code.

---
+++ webpack/route.js
@@ -0,0 +1,10 @@
+var routes = [{
+  name: 'index',
+  entry: './index/start.tsx',
+  plugins: {
+    template: './../templates/index.html',
+    filename: 'index.html'
+  }
+}]
+
+module.exports = routes;
